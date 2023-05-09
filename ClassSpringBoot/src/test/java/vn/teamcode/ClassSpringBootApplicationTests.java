fbba3a5b5924ffb682c3279c47434e95525dd68a
Zin <zinlee0pro@gmail.com>
2023-05-09 22:17:32 +0700

    First commit

---
+++ ClassSpringBoot/src/test/java/vn/teamcode/ClassSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.teamcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
